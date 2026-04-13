49e76182dd48f4b2767a05fa2e4574727cc7a4c1
Jörgen <jorgen.karlsson.91@gmail.com>
2026-04-13 22:02:53 +0200

    first

---
+++ src/main/java/com/karlsson/gruppggateway/GruppGGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.karlsson.gruppggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppGGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GruppGGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karlsson/gruppggateway/GruppGGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlsson.gruppggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppGGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
